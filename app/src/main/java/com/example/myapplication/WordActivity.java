acb16848bcf6806cef38eca4ee46dd93b8b8db97
Đỗ Đức Dương <83938501+duongdd141100@users.noreply.github.com>
2023-06-09 11:02:31 +0700

    [9/6] Send notification

--- app/src/main/java/com/example/myapplication/WordActivity.java
+++ app/src/main/java/com/example/myapplication/WordActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.view.menu.MenuBuilder;
+import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,9 +12,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.Menu;
@@ -96,6 +101,9 @@ public class WordActivity extends AppCompatActivity {
             showProfile();
             requestLocationPermission();
         }
+        if (item.getItemId() == R.id.sendNoti) {
+            sendNotification();
+        }
         return super.onOptionsItemSelected(item);
 //        switch (item.getItemId()) {
 //            case R.id.settings:
@@ -155,7 +163,39 @@ public class WordActivity extends AppCompatActivity {
                 builder.show();
             }
             requestPermissions(new String[] {Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION}, 2);
+                Manifest.permission.ACCESS_FINE_LOCATION}, 3);
         }
+    }
+
+    private void sendNotification() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
+                != PackageManager.PERMISSION_GRANTED) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                        .setTitle("Reason for request send notification")
+                        .setMessage("Please grant send notification to receive")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        });
+                builder.show();
+            }
+            requestPermissions(new String[] {Manifest.permission.POST_NOTIFICATIONS}, 3);
+        }
+        String channelId = "001";
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId)
+                .setAutoCancel(false)
+                .setSmallIcon(R.drawable.ic_action_favourite)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentTitle("Demo Noti")
+                .setContentText("Hello! this is demo message");
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(channelId, "demo chanel", NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+        notificationManager.notify(2, builder.build());
     }
 }
